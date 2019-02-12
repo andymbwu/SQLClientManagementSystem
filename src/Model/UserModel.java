803a81b302904135686dd1e62053091ed04c54cb
Andy Wu <andymbwu@gmail.com>
2019-02-11 19:52:30 -0700

    updated javadocs 2

--- src/Model/UserModel.java
+++ src/Model/UserModel.java
@@ -11,6 +11,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Class that contains user info for a single user
+ */
 public class UserModel implements Serializable {
 
 	private int id;
@@ -20,9 +23,22 @@ public class UserModel implements Serializable {
 	private String postalCode;
 	private String phoneNumber;
 	private String userType;
+	private static final long serialVersionUID = 1L;
 
+	/**
+	 * default constructor
+	 */
 	public UserModel(){}
 
+	/**
+	 * Constructor that assigns user info to member variables, no ID
+	 * @param firstName: User first name
+	 * @param lastName: User last name
+	 * @param address: User address
+	 * @param postalCode: User postal code
+	 * @param phoneNumber: User phone number
+	 * @param userType: Type of users (residential/commercial)
+	 */
 	public UserModel(String firstName, String lastName, String address, String postalCode, 
 			String phoneNumber, String userType)
 	{
@@ -33,7 +49,17 @@ public class UserModel implements Serializable {
 		this.phoneNumber = phoneNumber;
 		this.userType = userType;
 	}
-	
+
+	/**
+	 * UserModel constructor that initializes user that has an ID
+	 * @param id
+	 * @param firstName
+	 * @param lastName
+	 * @param address
+	 * @param postalCode
+	 * @param phoneNumber
+	 * @param clientType
+	 */
 	public UserModel(int id, String firstName, String lastName, String address, String postalCode, String phoneNumber, String clientType){
 		setId(id);
 		setFirstName(firstName);
@@ -44,58 +70,123 @@ public class UserModel implements Serializable {
 		setUserType(clientType);
 	}
 
-	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Setter for user id
+	 * @param id: user id
+	 */
 	public void setId(int id){
 		this.id = id;
 	}
 
+	/**
+	 * setter for user first name
+	 * @param firstName: user first name
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+	/**
+	 * Setter for user last name
+	 * @param lastName: user last name
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+	/**
+	 * Setter for user address
+	 * @param address: user address
+	 */
 	public void setAddress(String address) {
 		this.address = address;
 	}
 
+	/**
+	 * Setter for user postal code
+	 * @param postalCode: user postal code
+	 */
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
 	}
 
+	/**
+	 * setter for user phone number
+	 * @param phoneNumber: user contact phone number
+	 */
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 
+	/**
+	 * setter for type of user (commercial/residential)
+	 * @param clientType: type of user
+	 */
 	public void setUserType(String clientType) {
 		this.userType = clientType;
 	}
 
+	/**
+	 * getter for user id
+	 * @return: user id
+	 */
 	public int getID(){
 		return this.id;
 	}
+
+	/**
+	 * getter for user first name
+	 * @return: user first name
+	 */
 	public String getFirstName(){
 		return this.firstName;
 	}
+
+	/**
+	 * getter for user last name
+	 * @return: user last name
+	 */
 	public String getLastName() {
 		return this.lastName;
 	}
+
+	/**
+	 * getter for user address
+	 * @return: user address
+	 */
 	public String getAddress(){
 		return this.address;
 	}
+
+	/**
+	 * getter for user postal code
+	 * @return: user postal code
+	 */
 	public String getPostalCode(){
 		return this.postalCode;
 	}
+
+	/**
+	 * getter for user phone number
+	 * @return: user phone number
+	 */
 	public String getPhoneNumber(){
 		return this.phoneNumber;
 	}
+
+	/**
+	 * getter for user type
+	 * @return: user type (commercial/residential)
+	 */
 	public String getUserType(){
 		return this.userType;
 	}
 
+	/**
+	 * User toString method
+	 * @return
+	 */
 	public String toString()
 	{
 		String user = this.id + " " + this.firstName + " " + this.lastName + ", " + this.address +
--- src/Model/UserWrapper.java
+++ src/Model/UserWrapper.java
@@ -3,56 +3,98 @@ package Model;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * Class that wraps user info into an object that also contains an action id and query
+ */
 public class UserWrapper implements Constants, Serializable {
 
-	private int action;
-	private String query;
-	private ArrayList<UserModel> userList;
+    private int action;
+    private String query;
+    private ArrayList<UserModel> userList;
 
-	public UserWrapper() {
-		this.action=0;
-		this.query="";
-		this.userList=null;
-	}
+    /**
+     * Constructor for user wrapper class that sets args to null or 0
+     */
+    public UserWrapper() {
+        this.action = 0;
+        this.query = "";
+        this.userList = null;
+    }
 
-	public UserWrapper(UserModel user, String query, int action){
-		this.action = action;
-		this.query = query;
-		userList.add(user);
-	}
-	public int getAction() {
-		return action;
-	}
+    /**
+     * Constructor that passes user, query, and action values
+     *
+     * @param user
+     * @param query
+     * @param action
+     */
+    public UserWrapper(UserModel user, String query, int action) {
+        this.action = action;
+        this.query = query;
+        userList.add(user);
+    }
 
-	public void setAction(int action) {
-		this.action = action;
-	}
+    /**
+     * getter for action int
+     *
+     * @return
+     */
+    public int getAction() {
+        return action;
+    }
 
-	public String getQuery() {
-		return query;
-	}
+    /**
+     * setter for action int
+     *
+     * @param action
+     */
+    public void setAction(int action) {
+        this.action = action;
+    }
 
-	public void setQuery(String query) {
-		this.query = query;
-	}
+    /**
+     * getter for query string
+     *
+     * @return
+     */
+    public String getQuery() {
+        return query;
+    }
 
-	public ArrayList<UserModel> getUserList() {
-		return userList;
-	}
+    /**
+     * setter for query string
+     *
+     * @param query
+     */
+    public void setQuery(String query) {
+        this.query = query;
+    }
 
-	public void setUserList(ArrayList<UserModel> userList) {
-		this.userList = userList;
-	}
+    /**
+     * getter for temp user arraylist
+     *
+     * @return
+     */
+    public ArrayList<UserModel> getUserList() {
+        return userList;
+    }
 
+    /**
+     * setter for temp user arraylist
+     *
+     * @param userList
+     */
+    public void setUserList(ArrayList<UserModel> userList) {
+        this.userList = userList;
+    }
 
-	public void clear() {
-		this.action = 0;
-		this.query = "";
-		this.userList=null;
-	}
-	
-//	public String toString() {
-//		userList.get(0)
-//	}
-	
+    /**
+     * method that clears the user wrapper
+     */
+    public void clear() {
+        this.action = 0;
+        this.query = "";
+        this.userList = null;
+    }
+
 }
